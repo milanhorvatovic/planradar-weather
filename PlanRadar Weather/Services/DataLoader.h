6cb475af217e0e6e73acb16c0973e16653c8059d
Milan Horvatovic <imsoft.labs@gmail.com>
2019-12-19 22:14:59 +0100

    [+] DataLoader

---
+++ PlanRadar Weather/Services/DataLoader.h
@@ -0,0 +1,37 @@
+//
+//  DataLoader.h
+//  PlanRadar Weather
+//
+//  Created by worker on 19/12/2019.
+//  Copyright © 2019 Milan Horvatovic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol NetworkEngine;
+@class ModelServiceWeather;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol WeatherDataLoader
+
+typedef void (^DataLoaderWeatherCompletition) (ModelServiceWeather * _Nullable object, NSError * _Nullable error);
+
+- (void)loadWeatherWithName:(NSString *)name
+               completition:(DataLoaderWeatherCompletition)completition;
+
+@end
+
+
+@interface DataLoader : NSObject
+
+- (instancetype)initWithBasePath:(NSString *)base
+                          engine:(id<NetworkEngine>)engine;
+
+@end
+
+@interface DataLoader (Weather) <WeatherDataLoader>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PlanRadar Weather/Services/DataLoader.m
@@ -0,0 +1,119 @@
+//
+//  DataLoader.m
+//  PlanRadar Weather
+//
+//  Created by worker on 19/12/2019.
+//  Copyright © 2019 Milan Horvatovic. All rights reserved.
+//
+
+#import "DataLoader.h"
+
+#import "NetworkEngine.h"
+#import "ModelServiceWeather.h"
+
+@import Mantle;
+
+NSString *const _apiKeyConstant = @"d982cf2f779410e5a9e80342b414d39f";
+
+@interface DataLoader ()
+
+@property (nonnull, readwrite, strong, nonatomic) NSURL *url;
+@property (nonnull, readwrite, strong, nonatomic) id<NetworkEngine> engine;
+
+@end
+
+@interface DataLoader (Private)
+
+- (Request *)_constructRequestWithRelativePath:(NSString *)relativePath
+                                    attributes:(NSDictionary *)queryAttributes;
+
+@end
+
+@interface DataLoader (PrivateLoad)
+
+typedef void (^DataLoaderCompletition) (id _Nullable object, NSError * _Nullable error);
+
+- (void)_loadWithRequest:(Request *)request
+                   class:(Class)class
+            completition:(DataLoaderCompletition)completition;
+
+@end
+
+@implementation DataLoader
+
+- (instancetype)initWithBasePath:(NSString *)base
+                          engine:(id<NetworkEngine>)engine {
+    NSURL *url = [NSURL URLWithString:base];
+    if (!url) {
+        @throw [NSException exceptionWithName:@"Init error"
+                                       reason:@"Base path is invalid"
+                                     userInfo:nil];
+    }
+    if (self = [super init]) {
+        self.url = url;
+        self.engine = engine;
+    }
+    return self;
+}
+
+@end
+
+@implementation DataLoader (Private)
+
+- (Request *)_constructRequestWithRelativePath:(NSString *)relativePath
+                                    attributes:(NSDictionary *)queryAttributes {
+    NSMutableDictionary *attributes = [@{@"appid": _apiKeyConstant} mutableCopy];
+    [attributes addEntriesFromDictionary:queryAttributes];
+    NSMutableArray<NSURLQueryItem *> *queryItems = [[NSMutableArray alloc] init];
+    for (NSString *key in attributes) {
+        [queryItems addObject:[[NSURLQueryItem alloc] initWithName:key value:attributes[key]]];
+    }
+    return [[Request alloc] initWithBaseURL:self.url
+                               relativePath:relativePath
+                                 queryItems:queryItems];
+}
+
+@end
+
+@implementation DataLoader (Weather)
+
+- (void)loadWeatherWithName:(NSString *)name
+               completition:(DataLoaderWeatherCompletition)completition {
+    Request *request = [self _constructRequestWithRelativePath:@"/weather"
+                                                    attributes:@{@"q": name}];
+    [self _loadWithRequest:request
+                     class:ModelServiceWeather.class
+              completition:^(id  _Nullable object, NSError * _Nullable error) {
+        if (error) {
+            completition(nil, error);
+            return;
+        }
+        completition(object, nil);
+    }];
+}
+
+@end
+
+@implementation DataLoader (PrivateLoad)
+
+- (void)_loadWithRequest:(Request *)request
+                   class:(Class)class
+            completition:(DataLoaderCompletition)completition {
+    [self.engine performRequest:request
+                   completition:^(NSDictionary * _Nullable json, NSError * _Nullable error) {
+        if (error) {
+            completition(nil, error);
+            return;
+        }
+        id object = [MTLJSONAdapter modelOfClass:class
+                              fromJSONDictionary:json
+                                           error:&error];
+        if (error) {
+            completition(nil, error);
+            return;
+        }
+        completition(object, nil);
+    }];
+}
+
+@end
